c06af581ebd8ffbe5839a1229a2e1a97ed8f4537
梁深南 <110004355+Draymond-Sreike@users.noreply.github.com>
2022-08-03 12:19:42 +0800

    Create Test_220803.java

---
+++ 2022.08.03/Test_220803.java
@@ -0,0 +1,13 @@
+public class Test_220803
+{
+	public static void main(String[] args)
+	{
+		int a = (int)4.14f;
+		System.out.println(a);
+//		100;
+		long num = 100L;
+		switch((int)num)
+		{
+		}
+	}	
+}
